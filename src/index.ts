6131f09f73a6a3cca333eaf8542b1dcea188d1f4
Rollphes <hiroto2054.314@gmail.com>
2023-09-03 13:52:55 +0900

    [fix]index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,29 @@
 import { Client } from '@/client/Client'
-import { EnkaManager } from '@/client/EnkaManager'
+import { EnkaData, EnkaManager } from '@/client/EnkaManager'
 import { AssetsNotFoundError } from '@/errors/AssetsNotFoundError'
 import { EnkaManagerError } from '@/errors/EnkaManagerError'
 import { EnkaNetworkError } from '@/errors/EnkaNetWorkError'
 import { ImageNotFoundError } from '@/errors/ImageNotFoundError'
 import { TextMapFormatError } from '@/errors/TextMapFormatError'
-import { Artifact } from '@/models/Artifact'
-import { ArtifactType } from '@/models/Artifact'
+import { Artifact, ArtifactType } from '@/models/Artifact'
 import { ImageAssets } from '@/models/assets/ImageAssets'
 import { Character } from '@/models/character/Character'
 import { Costume } from '@/models/character/Costume'
 import { FightProp } from '@/models/character/FightProp'
-import { Profile } from '@/models/character/Profile'
-import { AssocType } from '@/models/character/Profile'
+import { AssocType, Profile } from '@/models/character/Profile'
 import { Skill } from '@/models/character/Skill'
 import { Talent } from '@/models/character/Talent'
 import { AvatarInfo } from '@/models/enka/AvatarInfo'
 import { PlayerInfo } from '@/models/enka/PlayerInfo'
 import { ShowAvatarInfo } from '@/models/enka/ShowAvatarInfo'
-import { Material } from '@/models/Material'
-import { MaterialType } from '@/models/Material'
-import { ItemType } from '@/models/Material'
+import { ItemType, Material, MaterialType } from '@/models/Material'
 import { FightPropType, StatProperty } from '@/models/StatProperty'
-import { Weapon } from '@/models/Weapon'
-import { WeaponType } from '@/models/Weapon'
-import { ClientOption } from '@/types'
-import { Element } from '@/types'
+import { Weapon, WeaponType } from '@/models/Weapon'
+import { ClientOption, Element } from '@/types'
 export {
   Client,
   EnkaManager,
+  EnkaData,
   AssetsNotFoundError,
   EnkaManagerError,
   EnkaNetworkError,
