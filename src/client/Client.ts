5156a0b71e3beeb672dde79c3465f01c55e3ef88
Rollphes <hiroto2054.314@gmail.com>
2023-10-01 05:02:34 +0900

    [fix]default imageBaseUrlByRegex in clientOption

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -46,7 +46,7 @@ export class Client extends AssetCacheManager {
         'https://enka.network/ui': [
           /^UI_(Costume|NameCardIcon|NameCardPic|RelicIcon|AvatarIcon_Side)_/,
           /^UI_AvatarIcon_(.+)_Card$/,
-          /^UI_EquipIcon_(.+)_Awaken$/,
+          /^UI_EquipIcon_(.+)$/,
         ],
         'https://res.cloudinary.com/genshin/image/upload/sprites': [
           /^Eff_UI_Talent_/,
