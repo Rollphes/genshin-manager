dc3b1aba320a4ae944fb07ee36a4a3982e3e184d
Rollphes <>
2025-08-12 03:57:28 +0900

    [refactor] Reorganize imports and streamline test logic

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,39 +1,35 @@
 import { Client } from '@/client/Client.js'
-import { CharacterInfo } from '@/models/character/CharacterInfo.js'
+import { EnkaManager } from '@/client/EnkaManager.js'
+import { Artifact } from '@/models/Artifact.js'
+import { CharacterConstellation } from '@/models/character/CharacterConstellation.js'
 import { CharacterCostume } from '@/models/character/CharacterCostume.js'
+import { CharacterInfo } from '@/models/character/CharacterInfo.js'
+import { CharacterInherentSkill } from '@/models/character/CharacterInherentSkill.js'
 import { CharacterProfile } from '@/models/character/CharacterProfile.js'
 import { CharacterSkill } from '@/models/character/CharacterSkill.js'
-import { CharacterInherentSkill } from '@/models/character/CharacterInherentSkill.js'
-import { CharacterConstellation } from '@/models/character/CharacterConstellation.js'
 import { CharacterStory } from '@/models/character/CharacterStory.js'
 import { CharacterVoice } from '@/models/character/CharacterVoice.js'
-import { Artifact } from '@/models/Artifact.js'
+import { DailyFarming } from '@/models/DailyFarming.js'
 import { Material } from '@/models/Material.js'
-import { Weapon } from '@/models/weapon/Weapon.js'
 import { Monster } from '@/models/Monster.js'
 import { ProfilePicture } from '@/models/ProfilePicture.js'
-import { DailyFarming } from '@/models/DailyFarming.js'
-import { EnkaManager } from '@/client/EnkaManager.js'
+import { Weapon } from '@/models/weapon/Weapon.js'
 
-const testType = process.env.npm_config_test_type
+const TEST_TYPE = process.env.npm_config_test_type
 
-async function main() {
+async function main(): Promise<void> {
   const client = new Client({
     downloadLanguages: ['EN'],
     defaultLanguage: 'EN',
   })
   await client.deploy()
-  console.log(`testType:${testType}`)
-  switch (testType) {
+  console.log(`testType:${TEST_TYPE}`)
+  switch (TEST_TYPE) {
     case 'AllId':
       console.log('Running test of class has AllId method...')
       CharacterInfo.allCharacterIds.forEach((id) => new CharacterInfo(id))
-      CharacterCostume.allCostumeIds.forEach(
-        (id) => new CharacterCostume(id),
-      )
-      CharacterProfile.allCharacterIds.forEach(
-        (id) => new CharacterProfile(id),
-      )
+      CharacterCostume.allCostumeIds.forEach((id) => new CharacterCostume(id))
+      CharacterProfile.allCharacterIds.forEach((id) => new CharacterProfile(id))
       CharacterSkill.allSkillIds.forEach((id) => new CharacterSkill(id))
       CharacterInherentSkill.allInherentSkillIds.forEach(
         (id) => new CharacterInherentSkill(id),
@@ -50,9 +46,8 @@ async function main() {
       ProfilePicture.allProfilePictureIds.forEach(
         (id) => new ProfilePicture(id),
       )
-      for (let i = 0; i < 7; i++) {
-        new DailyFarming(i)
-      }
+      for (let i = 0; i < 7; i++) void new DailyFarming(i)
+
       console.log('AllId test passed!')
       break
     case 'EnkaNetwork':
@@ -60,7 +55,7 @@ async function main() {
       await new EnkaManager().fetchAll(800802278)
       console.log('EnkaNetwork test passed!')
       break
-    case 'findMonsterIdByDescribeId':
+    case 'findMonsterIdByDescribeId': {
       console.log('Running test of findMonsterIdByDescribeId...')
       console.log('step 1:MonsterDescribeExcelConfigData keys test...')
       const describeIds = Object.keys(
@@ -68,48 +63,47 @@ async function main() {
       )
       describeIds.map((id) => {
         const monsterId = Monster.findMonsterIdByDescribeId(Number(id))
-        new Monster(monsterId)
+        void new Monster(monsterId)
       })
       console.log('findMonsterIdByDescribeId test passed!')
       break
-    case 'Image':
+    }
+    case 'Image': {
       console.log('Running test of Image...')
-      const artifactIds = await Artifact.allArtifactIds
+      const artifactIds = Artifact.allArtifactIds
       for (const id of artifactIds) {
         const artifact = new Artifact(id, 10001)
         await artifact.icon.fetchBuffer().catch((e) => console.log(e))
       }
 
-      const materialIds = await Material.allMaterialIds
+      const materialIds = Material.allMaterialIds
       for (const id of materialIds) {
         const material = new Material(id)
         await material.icon.fetchBuffer().catch((e) => console.log(e))
-        for (const img of material.pictures) {
+        for (const img of material.pictures)
           await img.fetchBuffer().catch((e) => console.log(e))
-        }
       }
 
-      const monsterIds = await Monster.allMonsterIds
+      const monsterIds = Monster.allMonsterIds
       for (const id of monsterIds) {
         const monster = new Monster(id)
         if (monster.icon === undefined) continue
         await monster.icon.fetchBuffer().catch((e) => console.log(e))
       }
 
-      const profilePictureIds = await ProfilePicture.allProfilePictureIds
+      const profilePictureIds = ProfilePicture.allProfilePictureIds
       for (const id of profilePictureIds) {
         const profilePicture = new ProfilePicture(id)
         await profilePicture.icon.fetchBuffer().catch((e) => console.log(e))
       }
 
-      const constellationIds =
-        await CharacterConstellation.allConstellationIds
+      const constellationIds = CharacterConstellation.allConstellationIds
       for (const id of constellationIds) {
         const constellation = new CharacterConstellation(id)
         await constellation.icon.fetchBuffer().catch((e) => console.log(e))
       }
 
-      const costumeIds = await CharacterCostume.allCostumeIds
+      const costumeIds = CharacterCostume.allCostumeIds
       for (const id of costumeIds) {
         const costume = new CharacterCostume(id)
         await costume.sideIcon.fetchBuffer().catch((e) => console.log(e))
@@ -118,20 +112,19 @@ async function main() {
         await costume.card.fetchBuffer().catch((e) => console.log(e))
       }
 
-      const inherentSkillIds =
-        await CharacterInherentSkill.allInherentSkillIds
+      const inherentSkillIds = CharacterInherentSkill.allInherentSkillIds
       for (const id of inherentSkillIds) {
         const inherentSkill = new CharacterInherentSkill(id)
         await inherentSkill.icon.fetchBuffer().catch((e) => console.log(e))
       }
 
-      const skillIds = await CharacterSkill.allSkillIds
+      const skillIds = CharacterSkill.allSkillIds
       for (const id of skillIds) {
         const skill = new CharacterSkill(id)
         await skill.icon.fetchBuffer().catch((e) => console.log(e))
       }
 
-      const weaponIds = await Weapon.allWeaponIds
+      const weaponIds = Weapon.allWeaponIds
       for (const id of weaponIds) {
         const weapon = new Weapon(id)
         await weapon.icon.fetchBuffer().catch((e) => console.log(e))
@@ -139,19 +132,24 @@ async function main() {
 
       console.log('Image test passed!')
       break
-    case 'Audio':
+    }
+    case 'Audio': {
       console.log('Running test of Audio...')
-      const voiceIds = await CharacterVoice.allFetterIds
+      const voiceIds = CharacterVoice.allFetterIds
       await Promise.all(
         ['JP', 'EN', 'CHS', 'KR'].map(async (cv) => {
           for (const id of voiceIds) {
-            const voice = new CharacterVoice(id, cv as 'EN' | 'KR' | 'JP' | 'CHS')
+            const voice = new CharacterVoice(
+              id,
+              cv as 'EN' | 'KR' | 'JP' | 'CHS',
+            )
             await voice.audio.fetchBuffer().catch((e) => console.log(e))
           }
         }),
       )
       console.log('Audio test passed!')
       break
+    }
   }
   process.exit(0)
 }
